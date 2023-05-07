8a2d70f835e6b7fa142f8a31a13ca49fb93886e8
mzapatal2 <m.zapatal2@uniandes.edu.co>
2023-05-07 02:12:42 -0500

    write_read

--- apirest/tasksCelery.py
+++ apirest/tasksCelery.py
@@ -46,6 +46,26 @@ def enviarCorreo(email_to):
         TIE_server.quit()
     return "función enviarCorreo terminada"
 
+def write_read(bucket_name, blob_name):
+    """Write and read a blob from GCS using file-like IO"""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your new GCS object
+    # blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    # Mode can be specified as wb/rb for bytes mode.
+    # See: https://docs.python.org/3/library/io.html
+    with blob.open("w") as f:
+        f.write("Hello world")
+
+    with blob.open("r") as f:
+        print(f.read())
+
 @appCelery.task(name='tasks.comprimir')
 def comprimir(filename, zipname, new_path, email_to, usuario_tarea, filenameCloud):
     print ('/n-> Se va a comprimir el archivo: {}'.format(filename))
