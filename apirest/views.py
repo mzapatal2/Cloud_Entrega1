74d1fe3b16d0987676aac6a55ce3c564be0707b1
mzapatal2 <m.zapatal2@uniandes.edu.co>
2023-05-07 22:23:21 -0500

    clous storage raiz

--- apirest/views.py
+++ apirest/views.py
@@ -230,8 +230,8 @@ class RecursoCarga(Resource):
             if file.filename == '':
                 print('No selected file')
             storage_client = storage.Client()
-            write_read('cloudentrega4', 'archivos/'+usuario+'/'+file.filename)
-            #write_read('cloudentrega4', file.filename)
+            #write_read('cloudentrega4', 'archivos/'+usuario+'/'+file.filename)
+            write_read('cloudentrega4', file.filename)
             file.save(PATH_FILE + usuario +'/'+ file.filename)
             return {'message':'Archivo Cargado'}
         except Exception as e:
